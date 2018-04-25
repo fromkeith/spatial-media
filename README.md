053266078cfc9a36c1498cead759f8725fac03ba
Keith <keith@picthrive.com>
2018-04-25 12:25:32 -0700

    remove more audio functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fork of google/spatial-media
-Intention to use this library to detect 360 video, instead of inject.
+
+* Intention to use this library to detect 360 video, instead of inject.
+* It does not interact with audio
+* It prints out video track data in a structured format
--- spatialmedia/metadata_utils.py
+++ spatialmedia/metadata_utils.py
@@ -85,14 +85,6 @@ SPHERICAL_TAGS_LIST = [
     "CroppedAreaTopPixels",
 ]
 
-SPATIAL_AUDIO_DEFAULT_METADATA = {
-    "ambisonic_order": 1,
-    "ambisonic_type": "periphonic",
-    "ambisonic_channel_ordering": "ACN",
-    "ambisonic_normalization": "SN3D",
-    "channel_map": [0, 1, 2, 3],
-}
-
 DEFAULT_XML_CONTENTS = """
 <rdf:SphericalVideo
   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
@@ -107,13 +99,10 @@ DEFAULT_XML_CONTENTS = """
 class Metadata(object):
     def __init__(self):
         self.video = None
-        self.audio = None
 
 class ParsedMetadata(object):
     def __init__(self):
         self.video = dict()
-        self.audio = None
-        self.num_audio_channels = 0
 
 SPHERICAL_PREFIX = "{http://ns.google.com/videos/1.0/spherical/}"
 SPHERICAL_TAGS = dict()
@@ -123,14 +112,6 @@ for tag in SPHERICAL_TAGS_LIST:
 integer_regex_group = "(\d+)"
 crop_regex = "^{0}$".format(":".join([integer_regex_group] * 6))
 
-
-def mpeg4_add_audio_metadata(mpeg4_file, in_fh, audio_metadata, console):
-    num_audio_tracks = get_num_audio_tracks(mpeg4_file, in_fh)
-    if num_audio_tracks > 1:
-        console("Error: Expected 1 audio track. Found %d" % num_audio_tracks)
-        return False
-
-    return mpeg4_add_spatial_audio(mpeg4_file, in_fh, audio_metadata, console)
 
 
 def parse_spherical_xml(contents, console):
@@ -215,26 +196,6 @@ def parse_spherical_mpeg4(mpeg4_file, fh, console):
                         metadata.video[trackName] = \
                             parse_spherical_xml(contents.decode("ascii", "ignore"), console)
 
-                if sub_element.name == mpeg.constants.TAG_MDIA:
-                    for mdia_sub_element in sub_element.contents:
-                        if mdia_sub_element.name != mpeg.constants.TAG_MINF:
-                            continue
-                        for stbl_elem in mdia_sub_element.contents:
-                            if stbl_elem.name != mpeg.constants.TAG_STBL:
-                                continue
-                            for stsd_elem in stbl_elem.contents:
-                                if stsd_elem.name != mpeg.constants.TAG_STSD:
-                                    continue
-                                for sa3d_container_elem in stsd_elem.contents:
-                                    if sa3d_container_elem.name not in \
-                                            mpeg.constants.SOUND_SAMPLE_DESCRIPTIONS:
-                                        continue
-                                    metadata.num_audio_channels = \
-                                        get_num_audio_channels(stsd_elem, fh)
-                                    for sa3d_elem in sa3d_container_elem.contents:
-                                        if sa3d_elem.name == mpeg.constants.TAG_SA3D:
-                                            sa3d_elem.print_box(console)
-                                            metadata.audio = sa3d_elem
     return metadata
 
 def parse_mpeg4(input_file, console):
@@ -282,131 +243,3 @@ def get_descriptor_length(in_fh):
         if (ord(size_byte) != int("0x80", 0)):
             break
     return descriptor_length
-
-
-def get_expected_num_audio_components(ambisonics_type, ambisonics_order):
-    """ Returns the expected number of ambisonic components for a given
-        ambisonic type and ambisonic order.
-    """
-    if (ambisonics_type == 'periphonic'):
-        return ((ambisonics_order + 1) * (ambisonics_order + 1))
-    else:
-        return -1
-
-def get_num_audio_channels(stsd, in_fh):
-    if stsd.name != mpeg.constants.TAG_STSD:
-        print("get_num_audio_channels should be given a STSD box")
-        return -1
-    for sample_description in stsd.contents:
-        if sample_description.name == mpeg.constants.TAG_MP4A:
-            return get_aac_num_channels(sample_description, in_fh)
-        elif sample_description.name in mpeg.constants.SOUND_SAMPLE_DESCRIPTIONS:
-            return get_sample_description_num_channels(sample_description, in_fh)
-    return -1
-
-def get_sample_description_num_channels(sample_description, in_fh):
-    """Reads the number of audio channels from a sound sample description.
-    """
-    p = in_fh.tell()
-    in_fh.seek(sample_description.content_start() + 8)
-
-    version = struct.unpack(">h", in_fh.read(2))[0]
-    revision_level = struct.unpack(">h", in_fh.read(2))[0]
-    vendor = struct.unpack(">i", in_fh.read(4))[0]
-    if version == 0:
-        num_audio_channels = struct.unpack(">h", in_fh.read(2))[0]
-        sample_size_bytes = struct.unpack(">h", in_fh.read(2))[0]
-    elif version == 1:
-        num_audio_channels = struct.unpack(">h", in_fh.read(2))[0]
-        sample_size_bytes = struct.unpack(">h", in_fh.read(2))[0]
-        samples_per_packet = struct.unpack(">i", in_fh.read(4))[0]
-        bytes_per_packet = struct.unpack(">i", in_fh.read(4))[0]
-        bytes_per_frame = struct.unpack(">i", in_fh.read(4))[0]
-        bytes_per_sample = struct.unpack(">i", in_fh.read(4))[0]
-    elif version == 2:
-        always_3 = struct.unpack(">h", in_fh.read(2))[0]
-        always_16 = struct.unpack(">h", in_fh.read(2))[0]
-        always_minus_2 = struct.unpack(">h", in_fh.read(2))[0]
-        always_0 = struct.unpack(">h", in_fh.read(2))[0]
-        always_65536 = struct.unpack(">i", in_fh.read(4))[0]
-        size_of_struct_only = struct.unpack(">i", in_fh.read(4))[0]
-        audio_sample_rate = struct.unpack(">d", in_fh.read(8))[0]
-        num_audio_channels = struct.unpack(">i", in_fh.read(4))[0]
-    else:
-        print("Unsupported version for " + sample_description.name + " box")
-        return -1
-
-    in_fh.seek(p)
-    return num_audio_channels
-
-def get_aac_num_channels(box, in_fh):
-    """Reads the number of audio channels from AAC's AudioSpecificConfig
-       descriptor within the esds child box of the input mp4a or wave box.
-    """
-    p = in_fh.tell()
-    if box.name not in [mpeg.constants.TAG_MP4A, mpeg.constants.TAG_WAVE]:
-        return -1
-
-    for element in box.contents:
-        if element.name == mpeg.constants.TAG_WAVE:
-            # Handle .mov with AAC audio, where the structure is:
-            #     stsd -> mp4a -> wave -> esds
-            channel_configuration = get_aac_num_channels(element, in_fh)
-            break
-
-        if element.name != mpeg.constants.TAG_ESDS:
-          continue
-        in_fh.seek(element.content_start() + 4)
-        descriptor_tag = struct.unpack(">c", in_fh.read(1))[0]
-
-        # Verify the read descriptor is an elementary stream descriptor
-        if ord(descriptor_tag) != 3:  # Not an MP4 elementary stream.
-            print("Error: failed to read elementary stream descriptor.")
-            return -1
-        get_descriptor_length(in_fh)
-        in_fh.seek(3, 1)  # Seek to the decoder configuration descriptor
-        config_descriptor_tag = struct.unpack(">c", in_fh.read(1))[0]
-
-        # Verify the read descriptor is a decoder config. descriptor.
-        if ord(config_descriptor_tag) != 4:
-            print("Error: failed to read decoder config. descriptor.")
-            return -1
-        get_descriptor_length(in_fh)
-        in_fh.seek(13, 1) # offset to the decoder specific config descriptor.
-        decoder_specific_descriptor_tag = struct.unpack(">c", in_fh.read(1))[0]
-
-        # Verify the read descriptor is a decoder specific info descriptor
-        if ord(decoder_specific_descriptor_tag) != 5:
-            print("Error: failed to read MP4 audio decoder specific config.")
-            return -1
-        audio_specific_descriptor_size = get_descriptor_length(in_fh)
-        assert audio_specific_descriptor_size >= 2
-        decoder_descriptor = struct.unpack(">h", in_fh.read(2))[0]
-        object_type = (int("F800", 16) & decoder_descriptor) >> 11
-        sampling_frequency_index = (int("0780", 16) & decoder_descriptor) >> 7
-        if sampling_frequency_index == 0:
-            # TODO: If the sample rate is 96kHz an additional 24 bit offset
-            # value here specifies the actual sample rate.
-            print("Error: Greater than 48khz audio is currently not supported.")
-            return -1
-        channel_configuration = (int("0078", 16) & decoder_descriptor) >> 3
-    in_fh.seek(p)
-    return channel_configuration
-
-
-def get_num_audio_tracks(mpeg4_file, in_fh):
-    """ Returns the number of audio track in the input mpeg4 file. """
-    num_audio_tracks = 0
-    for element in mpeg4_file.moov_box.contents:
-        if (element.name == mpeg.constants.TAG_TRAK):
-            for sub_element in element.contents:
-                if (sub_element.name != mpeg.constants.TAG_MDIA):
-                    continue
-                for mdia_sub_element in sub_element.contents:
-                    if (mdia_sub_element.name != mpeg.constants.TAG_HDLR):
-                        continue
-                    position = mdia_sub_element.content_start() + 8
-                    in_fh.seek(position)
-                    if (in_fh.read(4) == mpeg.constants.TAG_SOUN):
-                        num_audio_tracks += 1
-    return num_audio_tracks
